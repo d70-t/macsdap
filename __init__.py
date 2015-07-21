d854e657f81441770cdd1e85cacf18708878c37e
Tobias Kölling <tobias.koelling@physik.uni-muenchen.de>
2015-07-21 12:18:52 +0200

    added search capability

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
 import pydap.lib
 import pydap.client
 import os
+import json
+import httplib2
+import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-pydap.lib.CA_CERTS = os.path.join(basedir, 'cacerts.txt')
+CA_CERTS = os.path.join(basedir, 'cacerts.txt')
+pydap.lib.CA_CERTS = CA_CERTS
 
 class MACSdap(object):
     def __init__(self, key=None, host='https://macsserver.physik.uni-muenchen.de'):
@@ -14,6 +19,15 @@ class MACSdap(object):
         if self.key is not None:
             url += '?key=%s'%self.key
         return MACSdapDS(pydap.client.open_url(url))
+    def search(self, **kwargs):
+        kwargs = kwargs.copy()
+        for k,v in kwargs.items():
+            if isinstance(v, datetime.datetime):
+                kwargs[k] = (v - datetime.datetime(1970,1,1,0,0,0)).total_seconds()*1000.
+        url = '%s/query/%s'%(self.host, '/'.join('%s:%s'%i for i in sorted(kwargs.items())))
+        if self.key is not None:
+            url += '?key=%s'%self.key
+        return SearchResult(url, self)
 
 class MACSdapDS(object):
     def __init__(self, dataset):
@@ -71,3 +85,38 @@ class MACSdapVariable(object):
     def __dir__(self):
         return dir(self._variable)
 
+class SearchResult(object):
+    def __init__(self, baseRequest, macsdap):
+        self._baseRequest = baseRequest
+        self._macsdap = macsdap
+        self.h = httplib2.Http(ca_certs = CA_CERTS)
+        (resp, content) = self.h.request(baseRequest, 'GET')
+        data = json.loads(content)
+        self._count = data['count']
+        self._limit = None
+    @property
+    def count(self):
+        if self._limit is None:
+            return self._count
+        else:
+            return min(self._limit, self._count)
+    def limit(self, limit):
+        self._limit = limit
+        return self
+    def __iter__(self):
+        ofs = 0
+        step = 20
+        while True:
+            (resp, content) = self.h.request(self._baseRequest+'/OFS:%d/LIMIT:%d'%(ofs, step), 'GET')
+            data = json.loads(content)
+            if len(data['result']) == 0:
+                break
+            for i, res in enumerate(data['result'], ofs):
+                if self._limit is not None and i >= self._limit:
+                    return
+                yield self._macsdap[res['_oid']]
+            ofs = data['stop']
+    def __getitem__(self, index):
+        (resp, content) = self.h.request(self._baseRequest+'/OFS:%d/LIMIT:%d'%(index, 1), 'GET')
+        data = json.loads(content)
+        return self._macsdap[data['result'][0]['_oid']]
